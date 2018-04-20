d97803b12fcd0477945cd4d9fc6242e5c7e8524f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-19 23:11:40 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,12 @@
+p6df::modules::darwin::version() { echo "0.0.1" }
+p6df::modules::darwin::deps()    { 
+	ModuleDeps=()
+}
+
+p6df::modules::darwin::external() { 
+
+}
+
+p6df::modules::darwin::init() {
+}
+
