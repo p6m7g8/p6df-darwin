8d3f00c3e5eaae4a9bbac3f853ec10e1e9f04b6d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-03-01 09:11:31 -0500

    feat(deps): moves bat here, adds exa; moves greps to shell

--- init.zsh
+++ init.zsh
@@ -22,20 +22,11 @@ p6df::modules::darwin::external::brew() {
 
   brew install ack
   brew install ag
+  brew install bat
+  brew install exa
   brew install fasd
   brew install fd
-  brew install htop
-  brew install lsof
-  brew install exa
-
-  brew install bgrep
-  brew install cgrep
-  brew install grepcidr
-  brew install ngrep
-  brew install pgrep
-  brew install pdfgrep
-  brew install psgrep
-  brew install ripgrep-all
+  brew install rg
 }
 
 ######################################################################
@@ -61,4 +52,28 @@ p6df::modules::darwin::home::symlink() {
 p6df::modules::darwin::init() {
 
     alias ell='exa -lgHhSa'
-}
+    alias ll='ell'
+
+    alias l='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store"'
+    alias lara='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --extended --tree'
+    alias la='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --extended --tree --level=1'
+    alias lar='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --extended --tree --level=2'
+    alias larr='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --extended --tree --level=3'
+    alias larrr='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --extended --tree --level=4'
+    alias larrrr='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --extended --tree --level=5'
+
+    alias lb='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --sort=size'
+    alias lac='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --sort=accessed --time=accessed'
+    alias lch='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --sort=changed --time=changed'
+    alias lcr='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --sort=created --time=created'
+    alias lm='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --sort=modified --time=modified'
+    alias lt='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store" --modified --changed --created --accessed'
+
+    alias lra='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store|node_modules" --git-ignore --tree'
+    alias lr='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store|node_modules" --git-ignore --tree --level=2'
+    alias lrr='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store|node_modules" --git-ignore --tree --level=3'
+    alias lrrr='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store|node_modules" --git-ignore --tree --level=4'
+    alias lrrrr='exa --long --all --header --classify --group --color-scale --ignore-glob=".git|.DS_Store|node_modules" --git-ignore --tree --level=5'
+
+    alias c='bat --paging=never'
+} 
